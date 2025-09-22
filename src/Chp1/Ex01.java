e550b5565e9ef41aeecf29a95b0e5e6993c895c3
kwon5min02 <rnjs1737@gmail.com>
2025-09-22 15:37:53 +0900

    chapter 1

---
+++ src/Chp1/Ex01.java
@@ -0,0 +1,9 @@
+package Chp1;
+
+// Welcome!!, 자바 세계로 오신 것을 환영합니다 출력
+public class Ex01 {
+    public static void main(String[] args) {
+        System.out.println("Welcome!!");
+        System.out.println("자바 세계로 오신 것을 환영합니다.");
+    }
+}
---
+++ src/Chp1/Ex02.java
@@ -0,0 +1,9 @@
+package Chp1;
+
+// Sorry~~, 재밌어서 미안합니다~~ 출력
+public class Ex02 {
+    public static void main(String[] args) {
+        System.out.println("Sorry~~");
+        System.out.println("재밌어서 미안합니다.");
+    }
+}
